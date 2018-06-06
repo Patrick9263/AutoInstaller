ad3e682d7f903643d14eb33f341a644650c599a3
Smittward <psmith9263@gmail.com>
2018-06-06 18:59:16 -0400

    Add Google Chrome

--- Main.ps1
+++ Main.ps1
@@ -1,1 +1,30 @@
-Write-Host "Hellow World!"
+
+$html.Links |
+Where-Object { $_.class -eq 'product-url' } |
+Select-Object -ExpandProperty href
+
+Start-BitsTransfer -Source $html.text.ToString() "~/Downloads"
+
+################################### paths for each installer ###################################
+# $googleChromePath = "$env:TEMP\GoogleChrome.exe"
+# ################################################################################################
+
+# Write-Host "Begin downloading the files..."
+# Start-BitsTransfer -Source http://dl.google.com/chrome/install/chrome_installer.exe $googleChromePath
+# Write-Host "Finished Downloading programs"
+
+
+# Write-Host "Running the installers..."
+
+# Start-Process -FilePath $googleChromePath -ArgumentList "/silent /install" -Wait
+# Write-Host "Finished Installing Chrome"
+
+# Write-Host "Finished Installing programs"
+
+# Write-Host "Deleting installers..."
+
+# ################################### Delete the installers once they finish ###################################
+# Remove-Item -LiteralPath $googleChromePath
+
+Write-Host "Installers deleted"
+
