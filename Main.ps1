e64049142e1843e9167195fd3aa9cd8310cfb6ea
Smittward <psmith9263@gmail.com>
2018-06-06 18:59:51 -0400

    Add Google Chrome

--- Main.ps1
+++ Main.ps1
@@ -1,30 +1,24 @@
 
-$html.Links |
-Where-Object { $_.class -eq 'product-url' } |
-Select-Object -ExpandProperty href
-
-Start-BitsTransfer -Source $html.text.ToString() "~/Downloads"
-
 ################################### paths for each installer ###################################
-# $googleChromePath = "$env:TEMP\GoogleChrome.exe"
-# ################################################################################################
+$googleChromePath = "$env:TEMP\GoogleChrome.exe"
+################################################################################################
 
-# Write-Host "Begin downloading the files..."
-# Start-BitsTransfer -Source http://dl.google.com/chrome/install/chrome_installer.exe $googleChromePath
-# Write-Host "Finished Downloading programs"
+Write-Host "Begin downloading the files..."
+Start-BitsTransfer -Source http://dl.google.com/chrome/install/chrome_installer.exe $googleChromePath
+Write-Host "Finished Downloading programs"
 
 
-# Write-Host "Running the installers..."
+Write-Host "Running the installers..."
 
-# Start-Process -FilePath $googleChromePath -ArgumentList "/silent /install" -Wait
-# Write-Host "Finished Installing Chrome"
+Start-Process -FilePath $googleChromePath -ArgumentList "/silent /install" -Wait
+Write-Host "Finished Installing Chrome"
 
-# Write-Host "Finished Installing programs"
+Write-Host "Finished Installing programs"
 
-# Write-Host "Deleting installers..."
+Write-Host "Deleting installers..."
 
-# ################################### Delete the installers once they finish ###################################
-# Remove-Item -LiteralPath $googleChromePath
+################################### Delete the installers once they finish ###################################
+Remove-Item -LiteralPath $googleChromePath
 
 Write-Host "Installers deleted"
 
