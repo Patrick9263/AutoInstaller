2b0f7184891d17157afc8dd6fa3f8a13e7cfb84b
Smittward <psmith9263@gmail.com>
2018-06-12 18:41:32 -0400

    Add chrome

--- Main.ps1
+++ Main.ps1
@@ -1,6 +1,7 @@
 
 ################################### links for each installer ###################################
 $links = @(
+    "http://dl.google.com/chrome/install/chrome_installer.exe"
     "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
     "https://www.python.org/ftp/python/3.6.5/python-3.6.5.exe"
     "https://discordapp.com/api/download?platform=win"
@@ -14,7 +15,6 @@ $links = @(
     "https://streamlabs.com/slobs/download"
     "https://github-production-release-asset-2e65be.s3.amazonaws.com/23216272/52993848-638c-11e8-87c7-4d75fc1d0b18?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20180612%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20180612T223423Z&X-Amz-Expires=300&X-Amz-Signature=10e59c3f70c9438e6e1e30ef01e745e7c06eb9abb1b873e4b397bfc684b41862&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3DGit-2.17.1.2-64-bit.exe&response-content-type=application%2Foctet-stream"
     "https://cmake.org/files/v3.11/cmake-3.11.2-win64-x64.msi"
-    #"https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20NA.exe"
     "http://videolan.mirrors.hivelocity.net/vlc/3.0.2/win64/vlc-3.0.2-win64.exe"
     "https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.Installer.x64.exe"
     "https://us.download.nvidia.com/GFE/GFEClient/3.13.1.30/GeForce_Experience_v3.13.1.30.exe"
@@ -26,6 +26,7 @@ $links = @(
 
 ################################### paths for each installer ###################################
 $paths = @(
+    "$env:TEMP\GoogleChrome.exe"
     "$env:TEMP\steam.exe"
     "$env:TEMP\python.exe"
     "$env:TEMP\discord.exe"
@@ -44,7 +45,6 @@ $paths = @(
     "$env:TEMP\nvidia.exe"
     "$env:TEMP\node.msi"
     "$env:TEMP\gyazo.exe"
-    #"$env:TEMP\GoogleChrome.exe"
 )
 
 ################################################################################################
@@ -93,6 +93,7 @@ foreach ($path in $paths) {
 Write-Host "Installers deleted"
 
 #### Links: 
+# "https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20NA.exe"
 # "https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.1.0/qbittorrent_4.1.0_x64_setup.exe/download"
 # "https://dl.google.com/dl/android/studio/install/3.1.2.0/android-studio-ide-173.4720617-windows.exe"
 # "https://get.videolan.org/vlc/3.0.2/win64/vlc-3.0.2-win64.exe"
